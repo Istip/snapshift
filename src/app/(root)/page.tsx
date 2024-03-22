584cfcdd12023fb0fd7d0cd68b5dc60c403ea3f4
Pasztor Isti <isticsek@gmail.com>
2024-03-22 22:47:56 +0200

    add test user button

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <p>Home</p>
+
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
